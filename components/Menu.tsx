8a9c09e2a1044406e846d83915a507070d87ef8b
Developer27149 <564151947@qq.com>
2023-08-02 08:34:44 +0800

    feat: add menu icon

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,6 +1,10 @@
-import { BsWindowSidebar } from "react-icons/bs"
-
 import { ETabType } from "~types/common"
+import clsx from "clsx"
+import { AiOutlineAudio } from 'react-icons/ai'
+import { BsWindowSidebar } from "react-icons/bs"
+import { MdMultipleStop } from 'react-icons/md'
+import { TbWorldWww } from 'react-icons/tb'
+import { VscMultipleWindows } from 'react-icons/vsc'
 
 import Tooltip from "./Tooltip"
 
@@ -8,30 +12,37 @@ export default function Menu() {
   const groupType = [
     {
       intro: ETabType.all,
-      icon: <BsWindowSidebar />
+      icon: <BsWindowSidebar />,
+      color: 'blue'
     },
     {
       intro: ETabType.domain,
-      icon: <BsWindowSidebar />
+      icon: <TbWorldWww />,
+      color: 'blue'
     },
     {
       intro: ETabType.windowId,
-      icon: <BsWindowSidebar />
+      icon: <VscMultipleWindows />,
+      color: 'blue'
     },
     {
       intro: ETabType.audible,
-      icon: <BsWindowSidebar />
+      icon: <AiOutlineAudio />,
+      color: 'blue'
     },
     {
       intro: ETabType.status,
-      icon: <BsWindowSidebar />
+      icon: <MdMultipleStop />,
+      color: 'blue'
     }
   ]
   return (
-    <div className="flex flex-col p-2 bg-gray-50 py-4">
-      {groupType.map(({ intro, icon }) => (
+    <div className="flex flex-col p-2 bg-gray-50 py-4 gap-4">
+      {groupType.map(({ intro, icon,color }) => (
         <Tooltip key={intro} intro={intro}>
-          <div className="p-1 bg-blue-50 rounded-md text-blue-500 text-[16px]">
+          <div className={clsx("cursor-pointer p-1 bg-blue-50 rounded-md text-[16px] hover:text-blue-600 transition-all", {
+            'text-blue-500': true,
+          })}>
             {icon as any}
           </div>
         </Tooltip>
