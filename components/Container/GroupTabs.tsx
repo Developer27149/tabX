65611cff9c4bf1d3bee2eaeca8e348735eed7afc
Developer27149 <564151947@qq.com>
2023-12-17 23:40:02 +0800

    feat: add chrome tab groups raw colors

--- components/Container/GroupTabs.tsx
+++ components/Container/GroupTabs.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import { useEffect, useMemo, useState } from "react"
 import { CiMaximize1, CiMinimize1 } from "react-icons/ci"
 import { IoIosClose } from "react-icons/io"
@@ -18,18 +19,18 @@ export default function ({ tabs }: IProps) {
   const [groups, setGroups] = useState<TChromeGroup[]>([])
   const [currentGroupId, setCurrentGroupId] = useState<number>(0)
 
-  // a better group color map
-  const colorMap = {
-    red: "#f44336",
-    pink: "#e91e63",
-    purple: "#9c27b0",
-    deepPurple: "#673ab7",
-    indigo: "#3f51b5",
-    blue: "#2196f3",
-    lightBlue: "#03a9f4",
-    cyan: "#00bcd4",
-    teal: "#009688"
-  }
+  // a better group
+  const PopularColors = {
+    grey: "#808080",
+    blue: "#1a73e8",
+    red: "#d93025",
+    yellow: "#f9ab00",
+    green: "#188038",
+    pink: "#d01884",
+    purple: "#a142f4",
+    cyan: "#007b83",
+    orange: "#fa903e"
+  } as const
 
   const onRemoveGroups = (id: number) => () => {
     console.log("remove group", id)
@@ -57,29 +58,37 @@ export default function ({ tabs }: IProps) {
   return (
     <div className="pb-12 overflow-auto max-h-[450px] pr-2">
       <div className="flex gap-2 flex-wrap">
-        {groups.map(({ title, id, color, collapsed }) => {
-          return (
-            <div
-              key={id}
-              className="text-[16px] px-2 py-1 rounded-sm cursor-pointer text-white"
-              style={{
-                background: colorMap[color] ?? "#9c27b0"
-              }}>
-              {title}
-            </div>
-          )
-        })}
+        {groups
+          .sort((prev, cur) => (cur?.title && cur.title.length > 0 ? 1 : -1))
+          .map(({ title, id, color, collapsed }) => {
+            return (
+              <div
+                key={id}
+                onClick={() => setCurrentGroupId(id)}
+                className={clsx(
+                  "text-[16px] cursor-pointer text-white",
+                  title && title !== ""
+                    ? "rounded-md px-2 py-1"
+                    : "rounded-full w-8"
+                )}
+                style={{
+                  background: PopularColors[color] ?? "#9c27b0"
+                }}>
+                {title}
+              </div>
+            )
+          })}
       </div>
 
       {currentGroup && (
         <div className="mt-4">
           <div className="group flex gap-4 items-center bg-gray-50 rounded-md p-2">
-            <div>{currentGroup.collapsed ? "折叠" : "展开"}的分组</div>
+            <div>{currentGroup.title}</div>
             <div className="flex gap-4 items-center ml-auto">
               <Tooltip intro="解散分组下的标签">
                 <button>
                   <TbClearAll
-                    className="text-[16px] rounded-full text-blue-400 opacity-0 group-hover:opacity-100"
+                    className="text-[16px] rounded-full text-blue-400 opacity-0.1 group-hover:opacity-100 cursor-pointer"
                     onClick={() => {}}
                   />
                 </button>
@@ -87,9 +96,9 @@ export default function ({ tabs }: IProps) {
               <Tooltip intro="删除分组下的所有标签">
                 <button
                   onClick={onRemoveGroups(currentGroup.id)}
-                  className="text-[16px] p-.5 group-hover:bg-blue-500 rounded-full text-white">
+                  className="text-[16px] p-.5 rounded-full text-blue-300 border border-blue-400">
                   <Tooltip intro={"close"}>
-                    <IoIosClose />
+                    <IoIosClose className="cursor-pointer" />
                   </Tooltip>
                 </button>
               </Tooltip>
